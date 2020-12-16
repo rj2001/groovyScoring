8ef99bbc4fad50449e11dc07ce8355bb0c5650fa
rj2001 <rgojr2001@yahoo.com>
2020-12-15 19:31:13 -0600

    Initial commit

---
+++ src/main/groovy/com/wisport/scoring/ScoringApplication.groovy
@@ -0,0 +1,13 @@
+package com.wisport.scoring
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ScoringApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ScoringApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/wisport/scoring/ScoringApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.wisport.scoring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ScoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
