5d2690799dced3d96e56aa6f90694b156c2cd9d1
RJ.Ochmann <rj.ochmann@surescripts.com>
2021-03-23 09:40:47 -0500

    Add todo statements for scoring class

--- src/main/groovy/com/wisport/scoring/ScoreEvents.groovy
+++ src/main/groovy/com/wisport/scoring/ScoreEvents.groovy
@@ -1,4 +1,8 @@
 package com.wisport.scoring
 
 class ScoreEvents {
+    // TODO - get results for an event
+    // TODO - sort by gender, age group
+    // TODO - score by age group place, 100pts 1st, 99pts 2nd, ...
+    // TODO - store in member_event_result table
 }
